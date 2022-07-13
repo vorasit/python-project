0485533b8a084a2ea6580010a520f5602519bc1c
vorasit phusuwan <40907778+vorasit@users.noreply.github.com>
2022-07-13 20:54:31 +0700

    Update Program.py

--- tkinter-gui/Program.py
+++ tkinter-gui/Program.py
@@ -97,6 +97,33 @@ Radiobutton(text="Python",variable=language,value=1,command=showChoice).pack()
 Radiobutton(text="Java",variable=language,value=2,command=showChoice).pack()
 Radiobutton(text="PHP",variable=language,value=3,command=showChoice).pack()
 Radiobutton(text="C",variable=language,value=4,command=showChoice).pack()
+
+def showChoice1():
+    print(language1.get(),language2.get(),language3.get(),language4.get())
+    choice1 = language1.get()
+    choice2 = language2.get()
+    choice3 = language3.get()
+    choice4 = language4.get()
+    
+    if choice1 == 1:
+        Label(text="คุณเลือกภาษา Python").pack(anchor=W)
+    if choice2 == 1:
+        Label(text="คุณเลือกภาษา Java").pack(anchor=W)
+    if choice3 == 1:
+        Label(text="คุณเลือกภาษา PHP").pack(anchor=W)
+    if choice4 == 1:
+        Label(text="คุณเลือกภาษา C").pack(anchor=W)
+# 0 = ไม่เลือก  1 = เลือก
+language1 = IntVar()
+Checkbutton(text="Python",variable=language1).pack(anchor=W)
+language2 = IntVar()
+Checkbutton(text="Java",variable=language2).pack(anchor=W)
+language3 = IntVar()
+Checkbutton(text="PHP",variable=language3).pack(anchor=W)
+language4 = IntVar()
+Checkbutton(text="C",variable=language4).pack(anchor=W)
+Button(text="แสดงตัวเลือก",command=showChoice1).pack(anchor=W)
+
 #กำหนดขนาดหน้าจอและตำเเหน่งหน้าจอ
 root.geometry("600x500+100+400")# w*h+x+y
 
