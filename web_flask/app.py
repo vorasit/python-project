9f4edffa4da91c380713224c6ac029dda4f2a600
vorasit phusuwan <40907778+vorasit@users.noreply.github.com>
2022-07-27 17:54:14 +0700

    jinja template & image & send data & dictionary

--- web_flask/app.py
+++ web_flask/app.py
@@ -1,18 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return " <h1>Hello World </h1>"
+    data = {"name":"pinzaa","age":"30","salary":"100000"}
+    return render_template("index.html",mydata = data)
 
 @app.route('/about')
 def about():
-    return "<h1> เกี่ยวกับฉัน </h1>"
+    return render_template("about.html")
 
 @app.route('/admin')
 def profile():
-    return "<h1> สวัสดีครับ Admin  </h1>"
+    name = "pinzaa"
+    age = 26
+    return render_template("admin.html",myname = name, myage = age)
 
 @app.route('/user/<name>/<age>')
 def member(name,age):
