275c008b667e40f546c3de9db3d23edc1557e12c
vorasit phusuwan <40907778+vorasit@users.noreply.github.com>
2022-08-02 20:55:53 +0700

    app category
    create category and models

--- web_blog_django/myproject/blogs/views.py
+++ web_blog_django/myproject/blogs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from category.models import Category
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    categories = Category.objects.all()
+    return render(request,"index.html",{'categories':categories})
