fdeef933a2104d9efc62b9e38438d03472c369d9
vorasit phusuwan <40907778+vorasit@users.noreply.github.com>
2022-08-04 21:28:39 +0700

    แสดง blogs หน้าหลัก

--- web_blog_django/myproject/blogs/views.py
+++ web_blog_django/myproject/blogs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from category.models import Category
+from .models import Blogs
 # Create your views here.
 def index(request):
     categories = Category.objects.all()
-    return render(request,"index.html",{'categories':categories})
+    blogs = Blogs.objects.all()
+    return render(request,"index.html",{'categories':categories,'blogs':blogs})
